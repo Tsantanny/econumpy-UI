263a49ca6fcbb36d86ab88390b0ee692cdcf531d
Tsantanny <hei.tsanta.3@gmail.com>
2024-08-17 00:50:58 +0300

    update register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,123 +1,76 @@
 import "../styles/RegisterPage.css";
-import { useForm } from "react-hook-form";
-
+import logo from "../assets/images/logo.png";
 function RegisterPage() {
-  const nameInput = document.getElementById("validationServer02");
-  function validate(e) {
-    e.target.classList.add("is-validate");
-  }
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  function onSubmit(data) {
-    console.log(data);
-  }
   return (
     <>
-      <div className="register-container container-fluid row ">
-        <div className="side-hero col"></div>
-
-        <div className="app-desc col">
-          <h3 className="collapseOnMobile">
-            Fill the form and become part of our communities
-          </h3>
-        </div>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="col-5 needs-validation "
-        >
-          <h1 className="mb-3 loginTitle ">Welcome </h1>
-          <p className="text-muted toRegisterRef">
-            {"Already have an account?"}
-            <a className="text-primary m-2  ">sign in</a>
+      <div className="background"></div>
+      <section className="main-container ">
+        <section className="presentation text-white">
+          <img src={logo} alt="" className="logo" />
+          <h2 className="mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+            molestiae sit iste
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+            commodi minima a est animi saepe tempore sed excepturi dolorem
+            voluptatibus numquam, perferendis consequatur? Beatae totam numquam
           </p>
-
-          
-            <div className="row mr">
-              <div class="col">
-                <label for="validationServer02" class="form-label">
-                  name
-                </label>
+        </section>
+        <section className="register-container ">
+          <h2>Create Your Account</h2>
+          <p>
+            Already have an account ? <span>Login</span>
+          </p>
+          <form>
+            <div className="d-flex u-name gap-4">
+              <div className="form-floating  ">
                 <input
-                  onChange={validate}
-                  type="text"
-                  {...register("name", {
-                    required: true,
-                  })}
-                  class="form-control reg-btn-names "
-                  id="validationServer02"
+                  type="email"
+                  className={`form-control `}
+                  id="floatingInput"
+                  placeholder="name@example.com"
                   required
                 />
-                <div class="valid-feedback">Looks good!</div>
+                <label htmlFor="floatingInput">name</label>
               </div>
-              <div class="col">
-                <label for="validationServer02" class="form-label">
-                  name
-                </label>
+              <div className="form-floating  ">
                 <input
-                  onChange={validate}
-                  type="text"
-                  {...register("name", {
-                    required: true,
-                  })}
-                  class="form-control reg-btn-names "
-                  id="validationServer02"
+                  type="email"
+                  className={`form-control `}
+                  id="floatingInput"
+                  placeholder="name@example.com"
                   required
                 />
-                <div class="valid-feedback">Looks good!</div>
+                <label htmlFor="floatingInput">username</label>
               </div>
-              </div>
-            
-            {/* <div className="form-floating col">
+            </div>
+            <div className="form-floating ">
               <input
-                type="text"
-                className={`form-control ${errors.name && "is-invalid"}}`}
-                id="floatingInput2"
-                placeholder="name"
-                {...register("name", { required: true, pattern: /^\S+@\S+$/i })}
+                type="email"
+                className={`form-control `}
+                id="floatingInput"
+                placeholder="name@example.com"
+                required
               />
-              <label htmlFor="floatingInput2">name</label>
-              <p className="invalid-feedback">name is invalid</p>
-            </div> */}
-          <div className="form-floating m-2 row">
-            <input
-              type="email"
-              className={`form-control ${errors.email && "is-invalid"}}`}
-              id="floatingInput"
-              placeholder="name@example.com"
-              {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-              required
-            />
-            <label htmlFor="floatingInput">Email</label>
-            <p className="invalid-feedback">Email is invalid</p>
-          </div>
+              <label htmlFor="floatingInput">email</label>
+            </div>
+            <div className="form-floating ">
+              <input
+                type="email"
+                className={`form-control `}
+                id="floatingInput"
+                placeholder="name@example.com"
+                required
+              />
+              <label htmlFor="floatingInput">password</label>
+            </div>
 
-          <div className="form-floating m-2">
-            <input
-              type="password"
-              className={`form-control ${errors.password && "is-invalid"}`}
-              id="floatingPassword"
-              placeholder="Password"
-              {...register("password", { required: true })}
-            />
-            <label htmlFor="floatingPassword">Password</label>
-            <p className="invalid-feedback">Password is invalid</p>
-          </div>
-
-          <div className="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me" required /> accept our{" "}
-              <a href="#">terms & privacy</a>
-            </label>
-          </div>
-
-          <button className="w-100 btn primary-btn  btn-lg" type="submit">
-            Sign in
-          </button>
-        </form>
-      </div>
+            <button className="primary-btn">Submit</button>
+          </form>
+          <div className="extends"></div>
+        </section>
+      </section>
     </>
   );
 }
