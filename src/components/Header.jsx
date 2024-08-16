1b42923ac6696635856b87f578a172d14325105b
Tsantanny <hei.tsanta.3@gmail.com>
2024-08-16 18:07:56 +0300

    feat(dashboard): add header to dashboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,30 @@
-import "../styles/global-style.css"
-import "../styles/App.css"
+import "../styles/global-style.css";
+import "../styles/Header.css";
+import logo from "../assets/images/logo.png";
 
+function Header() {
+  return (
+    <div className="header">
+      <div className="container d-flex">
+        <img src={logo} className="dashboard-logo" alt="Logo" />{" "}
+        <div className="search-bar">
+          <input type="text" />
+          <button className="round-btn">
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+        
+        <div className="personnal">
+          <button className="round-btn">
+            <i className="fas fa-user"></i>
+          </button>
+          <button className="round-btn">
+            <i className="fas fa-bell"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import Home from "./pages/Home";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+    </>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
