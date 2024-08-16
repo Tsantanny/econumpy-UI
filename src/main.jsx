a0c688ebb3c2f074a22bb77ca0b510b7af5134aa
tsanta22kyle <hei.tsanta.2@gmail.com>
2024-08-16 17:13:08 +0300

    feat : inscription page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import RegisterPage from './pages/Register.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RegisterPage></RegisterPage>
   </React.StrictMode>,
 )
