5042b15d33d7931fa1ae03049271ec0027d62f6e
Tsantanny <hei.tsanta.3@gmail.com>
2024-08-16 18:07:56 +0300

    feat(dashboard): add header to dashboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,29 @@
-import "../styles/global-style.css"
-import "../styles/App.css"
+import "../styles/global-style.css";
+import "../styles/Header.css";
+import logo from "../assets/images/logo.png";
 
+function Header() {
+  return (
+    <div className="min-container header">
+      <div className="d-flex">
+        <img src={logo} className="dashboard-logo" alt="Logo" />{" "}
+        <div className="search-bar">
+          <input type="text" />
+          <button className="round-btn">
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+        <div className="personnal">
+          <button className="round-btn">
+            <i className="fas fa-user"></i>
+          </button>
+          <button className="round-btn">
+            <i className="fas fa-bell"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
