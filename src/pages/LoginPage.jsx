2afea15e7656d43fb128c87a41ea528ba9f44ea2
dieudonne19 <hei.dieudonne.19@gmail.com>
2024-08-17 06:30:19 +0300

    fix: change lorem ipsum texts

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,14 @@ import "../styles/global-style.css";
 import "../styles/RegisterPage.css";
 
 const LoginPage = () => {
-  const { handleSubmit, register, handleClick, navigateTo, handleRadioChange, isError } = useLogin();
+  const {
+    handleSubmit,
+    register,
+    handleClick,
+    navigateTo,
+    handleRadioChange,
+    isError,
+  } = useLogin();
 
   return (
     <>
@@ -13,14 +20,12 @@ const LoginPage = () => {
       <section className="main-container ">
         <section className="presentation text-white">
           <img src={logo} alt="" className="logo" />
-          <h2 className="mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
-            molestiae sit iste
-          </h2>
+          <h2 className="mt-4">Welcome Back to Our Community</h2>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
-            commodi minima a est animi saepe tempore sed excepturi dolorem
-            voluptatibus numquam, perferendis consequatur? Beatae totam numquam
+            Rejoin our efforts in fostering a healthier environment and a
+            thriving community. Together, we can create
+            lasting positive change. Let’s build a better future, one step at a
+            time
           </p>
         </section>
         <section className="register-container ">
@@ -58,7 +63,7 @@ const LoginPage = () => {
                 placeholder="Password"
                 {...register("password", {
                   required: true,
-                  minLength: { value: 6, message: "Password is required"},
+                  minLength: { value: 6, message: "Password is required" },
                 })}
               />
               <label htmlFor="floatingPassword">Password</label>
@@ -68,12 +73,30 @@ const LoginPage = () => {
             <div>
               <label>Account type :</label>
               <div className="form-check form-check-inline mx-3">
-                <input className="form-check-input" onChange={(e) => handleRadioChange(e)}  type="radio" name="category" id="inlineRadio1" value="individual"/>
-                <label className="form-check-label" htmlFor="inlineRadio1">Inidividual</label>
+                <input
+                  className="form-check-input"
+                  onChange={(e) => handleRadioChange(e)}
+                  type="radio"
+                  name="category"
+                  id="inlineRadio1"
+                  value="individual"
+                />
+                <label className="form-check-label" htmlFor="inlineRadio1">
+                  Inidividual
+                </label>
               </div>
               <div className="form-check form-check-inline">
-                <input className="form-check-input" onChange={(e) => handleRadioChange(e)}  type="radio" name="category" id="inlineRadio2" value="organization"/>
-                <label className="form-check-label" htmlFor="inlineRadio2">Organization</label>
+                <input
+                  className="form-check-input"
+                  onChange={(e) => handleRadioChange(e)}
+                  type="radio"
+                  name="category"
+                  id="inlineRadio2"
+                  value="organization"
+                />
+                <label className="form-check-label" htmlFor="inlineRadio2">
+                  Organization
+                </label>
               </div>
             </div>
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,21 +2,27 @@ import "../styles/RegisterPage.css";
 import logo from "../assets/images/logo.png";
 import { useRegister } from "../components/useRegister";
 function RegisterPage() {
-  const { handleSubmit, register, handleClick, errors, navigateTo, handleRadioChange } = useRegister()
+  const {
+    handleSubmit,
+    register,
+    handleClick,
+    errors,
+    navigateTo,
+    handleRadioChange,
+  } = useRegister();
   return (
     <>
       <div className="background"></div>
       <section className="main-container ">
         <section className="presentation text-white">
           <img src={logo} alt="" className="logo" />
-          <h2 className="mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
-            molestiae sit iste
-          </h2>
+          <h2 className="mt-4">Join Our Community for a Better Future</h2>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
-            commodi minima a est animi saepe tempore sed excepturi dolorem
-            voluptatibus numquam, perferendis consequatur? Beatae totam numquam
+            Be part of a community dedicated to sustainability, well-being, and
+            shared growth. Create your account today and contribute to a
+            platform focused on positive change. Together, we can build a
+            healthier environment and a stronger community. Register now to
+            begin your journey with us.
           </p>
         </section>
         <section className="register-container ">
@@ -38,7 +44,7 @@ function RegisterPage() {
                   className={`form-control `}
                   id="floatingInputName"
                   placeholder="Name"
-                  {...register('name')}
+                  {...register("name")}
                   required
                 />
                 <label htmlFor="floatingInputName">Name</label>
@@ -49,7 +55,7 @@ function RegisterPage() {
                   className={`form-control `}
                   id="floatingInputUsername"
                   placeholder="Username"
-                  {...register('username')}
+                  {...register("username")}
                   required
                 />
                 <label htmlFor="floatingInputUsername">Username</label>
@@ -61,7 +67,7 @@ function RegisterPage() {
                 className={`form-control `}
                 id="floatingInputEmail"
                 placeholder="name@example.com"
-                {...register('email')}
+                {...register("email")}
                 required
               />
               <label htmlFor="floatingInputEmail">Email</label>
@@ -72,7 +78,10 @@ function RegisterPage() {
                 className={`form-control `}
                 id="floatingInputPassword"
                 placeholder="Paswwordm"
-                {...register('password', {required: true, minLength: {value: 6}})}
+                {...register("password", {
+                  required: true,
+                  minLength: { value: 6 },
+                })}
                 required
               />
               <label htmlFor="floatingInputPassword">Password</label>
@@ -81,12 +90,30 @@ function RegisterPage() {
             <div>
               <label>Account type :</label>
               <div className="form-check form-check-inline mx-3">
-                <input className="form-check-input" onChange={(e) => handleRadioChange(e)}  type="radio" name="category" id="inlineRadio1" value="individual"/>
-                <label className="form-check-label" htmlFor="inlineRadio1">Inidividual</label>
+                <input
+                  className="form-check-input"
+                  onChange={(e) => handleRadioChange(e)}
+                  type="radio"
+                  name="category"
+                  id="inlineRadio1"
+                  value="individual"
+                />
+                <label className="form-check-label" htmlFor="inlineRadio1">
+                  Inidividual
+                </label>
               </div>
               <div className="form-check form-check-inline">
-                <input className="form-check-input" onChange={(e) => handleRadioChange(e)}  type="radio" name="category" id="inlineRadio2" value="organization"/>
-                <label className="form-check-label" htmlFor="inlineRadio2">Organization</label>
+                <input
+                  className="form-check-input"
+                  onChange={(e) => handleRadioChange(e)}
+                  type="radio"
+                  name="category"
+                  id="inlineRadio2"
+                  value="organization"
+                />
+                <label className="form-check-label" htmlFor="inlineRadio2">
+                  Organization
+                </label>
               </div>
             </div>
 
