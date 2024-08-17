42fe3676b971bbbba5c3ddd7f4421e73f90ac944
Tsantanny <hei.tsanta.3@gmail.com>
2024-08-17 03:56:59 +0300

    feat: add pop up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
 import Posts from "../components/Posts";
+import NewPost from "../components/NewPost";
 function Home() {
   return (
     <>
+      <NewPost></NewPost>
       <Header></Header>
       <SideBar></SideBar>
       <div
-        style={{ marginLeft: "300px", marginTop: "90px", marginRight: "350px" }}
+        style={{ marginLeft: "340px", marginTop: "90px", marginRight: "350px" }}
       >
+        <Posts></Posts>
+        <Posts></Posts>
+        <Posts></Posts>
         <Posts></Posts>
       </div>
     </>
